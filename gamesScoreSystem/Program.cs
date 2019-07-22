768d5818155661688b8b6828a1b42198d5b0a9de
xyqlx <bgxmy_b@163.com>
2019-07-22 21:11:47 +0800

    初始化项目

--- gamesScoreSystem/Program.cs
+++ gamesScoreSystem/Program.cs
@@ -10,6 +10,17 @@ namespace gamesScoreSystem
     {
         static void Main(string[] args)
         {
+            Welcome();
+            Console.ReadKey();
+        }
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to Games Score System. " +
+                "Commands end with endline.\n" +
+                "Your can only open one instance.\n" +
+                "System version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "\n" +
+                "\nCopyright (c) 2019, xyqlx. All rights reserved.\n" +
+                "\nType 'help' for help. Type 'clear' to clear the screen.\n");
         }
     }
 }
