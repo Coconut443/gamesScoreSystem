957e1fa70d39961cfe651456065fb9fd242d5a5c
xyqlx <bgxmy_b@163.com>
2019-07-22 20:45:09 +0800

    添加项目文件。

---
+++ gamesScoreSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamesScoreSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
