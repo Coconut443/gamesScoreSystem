2b9dfcacb4b2260af866886750fa600353b54022
xyqlx <bgxmy_b@163.com>
2019-07-28 22:22:25 +0800

    完成了部分执行内容
    注：同样没有测试，仅仅是因为今天最后一次提交（

--- gamesScoreSystem/PreQuery.cs
+++ gamesScoreSystem/PreQuery.cs
@@ -38,6 +38,8 @@ namespace gamesScoreSystem
         private StreamType streamType = StreamType.None;
         private Entity entity;
         private SortedSet<int> resultIndex;
+        private SortedSet<int> resultInt;
+        private SortedSet<string> resultString;
         private int resultNum = 0;
         public PreQuery(Session session)
         {
@@ -84,32 +86,32 @@ namespace gamesScoreSystem
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.id, null, function.Params[0]));
                     break;
                 case "eq":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
-                        throw new Exception("eq函数参数应为(field,<num>)");
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam) || !(function.Params[1] is StringParam))
+                        throw new Exception("eq函数参数应为(field,<num>/\"string\")");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.eq, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
                 case "gt":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam))
                         throw new Exception("gt函数参数应为(field,<num>)");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.gt, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
                 case "lt":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam))
                         throw new Exception("lt函数参数应为(field,<num>)");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.lt, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
                 case "ne":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam))
                         throw new Exception("ne函数参数应为(field,<num>)");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.ne, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
                 case "gte":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam))
                         throw new Exception("gte函数参数应为(field,<num>)");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.gte, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
                 case "lte":
-                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam))
+                    if (function.Params.Count != 2 || !(function.Params[0] is IdParam) || !(function.Params[1] is NumParam) || !(function.Params[1] is QueryParam))
                         throw new Exception("lte函数参数应为(field,<num>)");
                     queryFilters.Add(new Tuple<QueryFilterType, Field, Param>(QueryFilterType.lte, GetField(function.Params[0] as IdParam), function.Params[1]));
                     break;
@@ -172,10 +174,145 @@ namespace gamesScoreSystem
         public void Exec()
         {
             //第一阶段，进行多轮筛选
+            resultIndex = new SortedSet<int>();
+            for (int i = 1; i <= entity.Length; ++i)
+                resultIndex.Add(i);
+            foreach(var filter in queryFilters)
+            {
+                var queryType = filter.Item1;
+                var field = filter.Item2;
+                var param = filter.Item3;
+                bool isNum = (param is NumParam);
+                bool isString = (param is StringParam);
+                bool isIdField = (field == null);
+                bool isIntField = (field is IntField);
+                bool isCharField = (field is CharField);
+                int num = isNum ? (param as NumParam).Value : 0;
+                string str = isString ? (param as StringParam).Value : "";
+                PreQuery query = (isNum || isString) ? null : (param as QueryParam).Value;
+                var intField = isIntField ? (field as IntField) : null;
+                var charField = isCharField ? (field as CharField) : null;
+                if (isNum && !(isIntField || isIdField))
+                    throw new Exception("int型的字段必须提供int参数");
+                else if (isString && !isCharField)
+                    throw new Exception("char(n)型的字段必须提供字符串参数");
+                else if (isIdField && query.entity != entity)
+                    throw new Exception("不同实体的id不能进行比较");
+                //无论如何都会执行
+                query.Exec();
+                switch (queryType)
+                {
+                    case QueryFilterType.id:
+                        if(isNum)
+                        {
+                            resultIndex.Clear();
+                            if (resultIndex.Contains(num))
+                                resultIndex.Add(num);
+                        }else
+                        {
+                            resultIndex.RemoveWhere(x => !query.resultIndex.Contains(x));
+                        }
+                        break;
+                    case QueryFilterType.eq:
+                        if (isNum)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => (isIdField ? x : intField.Data[x]) == num));
+                        else if (isString)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => charField[x] == str));
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.IntersectWith(query.resultIndex);
+                            else if (isIntField)
+                                resultIndex = new SortedSet<int>(resultIndex.Where(x => query.resultInt.Contains(intField.Data[x])));
+                            else
+                                resultIndex = new SortedSet<int>(resultIndex.Where(x => query.resultString.Contains(charField[x])));
+                        }
+                        break;
+                    case QueryFilterType.gt:
+                        if (isNum)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => (isIdField ? x : intField.Data[x]) > num));
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.RemoveWhere(x => x <= query.resultIndex.Min);
+                            else if (isIntField)
+                                resultIndex.RemoveWhere(x => intField.Data[x] <= query.resultInt.Min);
+                        }
+                        break;
+                    case QueryFilterType.lt:
+                        if (isNum)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => (isIdField ? x : intField.Data[x]) < num));
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.RemoveWhere(x => x >= query.resultIndex.Min);
+                            else if (isIntField)
+                                resultIndex.RemoveWhere(x => intField.Data[x] >= query.resultInt.Min);
+                        }
+                        break;
+                    case QueryFilterType.ne:
+                        if (isNum)
+                            resultIndex.RemoveWhere(x => (isIdField ? x : intField.Data[x]) == num);
+                        else if (isString)
+                            resultIndex.RemoveWhere(x => charField[x] == str);
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.ExceptWith(query.resultIndex);
+                            else if (isIntField)
+                                resultIndex.RemoveWhere(x => query.resultInt.Contains(intField.Data[x]));
+                            else
+                                resultIndex.RemoveWhere(x => query.resultString.Contains(charField[x]));
+                        }
+                        break;
+                    case QueryFilterType.gte:
+                        if (isNum)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => (isIdField ? x : intField.Data[x]) >= num));
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.RemoveWhere(x => x < query.resultIndex.Min);
+                            else if (isIntField)
+                                resultIndex.RemoveWhere(x => intField.Data[x] < query.resultInt.Min);
+                        }
+                        break;
+                    case QueryFilterType.lte:
+                        if (isNum)
+                            resultIndex = new SortedSet<int>(resultIndex.Where(x => (isIdField ? x : intField.Data[x]) <= num));
+                        else
+                        {
+                            if (isIdField)
+                                resultIndex.RemoveWhere(x => x > query.resultIndex.Min);
+                            else if (isIntField)
+                                resultIndex.RemoveWhere(x => intField.Data[x] > query.resultInt.Min);
+                        }
+                        break;
+                    case QueryFilterType.contain:
+                        break;
+                    case QueryFilterType.regex:
+                        break;
+                }
+            }
             //第二阶段，排序
-            //第三阶段，跳过
+            //第三阶段，skip和limit
             //第四阶段，计算
-            //第五阶段，输出
+            //第五阶段，综合
+            if(this.fields.Count == 1)
+            {
+                if(this.fields[0] is IntField)
+                {
+                    var intField = (this.fields[0] as IntField);
+                    resultInt = new SortedSet<int>();
+                    foreach (var index in resultIndex)
+                        resultInt.Add(intField.Data[index]);
+                }else if(this.fields[0] is CharField)
+                {
+                    var charField = (this.fields[0] as CharField);
+                    resultInt = new SortedSet<int>();
+                    foreach (var index in resultIndex)
+                        resultString.Add(charField[index]);
+                }
+            }
         }
     }
 
